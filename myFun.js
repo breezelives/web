e0f85f3820eb942bdbc3104efab2c1562d12e1e0
Breeze <fengfulife@gmail.com>
2017-05-08 16:34:44 +0800

    Update myFun.js

--- myFun.js
+++ myFun.js
@@ -66,5 +66,5 @@ function palindrome(str) {
 function titleCase(str){
   return str.toLowerCase().split(' ').map(function(item){
     return item[0].toUpperCase() + item.slice(1);
-  })
+  }).join(' ');
 }
