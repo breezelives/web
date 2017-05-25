07aa84a3e1bbe3d46070599522de1a8d8039ede6
Breeze <fengfulife@gmail.com>
2017-05-25 15:30:34 +0800

    Update freeCodeCamp.js

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,11 +1,19 @@
 function where(collection, source){
-		var arr = []
-		collection.map(function(item){
-			var sign = 1
-			Object.keys(source).map(function(v){
-				if (!item.hasOwnProperty(v) || source[v] != item[v]) sign = 0
-			})
-			if(sign === 1) arr.push(item)
+	var arr = []
+	collection.map(function(item){
+		var sign = 1
+		Object.keys(source).map(function(v){
+			if (!item.hasOwnProperty(v) || source[v] != item[v]) sign = 0
 		})
-	    return arr
+		if(sign === 1) arr.push(item)
+	})
+    return arr
+}
+
+function myReplace(str, before, after){
+	var firstLetter = before.charAt(0)
+	if(firstLetter >= 'A' && firstLetter <= 'Z' ) {
+		after = after[0].toUpperCase() + after.substr(1)
 	}
+	return str.replace(before, after)
+}
