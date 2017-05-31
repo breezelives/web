79610f42e68e774b62b941c5efa4c63e06cb23c8
Breeze <fengfulife@gmail.com>
2017-05-31 16:04:53 +0800

    Update freeCodeCamp.js

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,1 +1,12 @@
 
+function translate(str){
+			var vowels = ['a', 'e', 'i', 'o', 'u']
+			if(vowels.indexOf(str[0]) === -1){
+				while(vowels.indexOf(str[0]) === -1){
+					str = str.substr(1) + str[0]
+				}
+			} else {
+				str += 'w'
+			}
+			return str + 'ay';
+		}
